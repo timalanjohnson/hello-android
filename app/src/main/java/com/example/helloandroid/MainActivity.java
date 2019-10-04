a33ed319478abec9dbc3cf3479f0d28865207844
roy <17001873@gmail.com>
2019-10-04 15:31:19 +0200

    hello i added a image

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.helloandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button hello;
+    TextView hey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hello = (Button) findViewById(R.id.BtnHello);
+        hey = (TextView) findViewById(R.id.hey);
+
+
     }
+
 }
+
+
