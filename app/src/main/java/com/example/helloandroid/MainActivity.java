9582e96f4e5f6873b30ea48d9e47d79d80902486
roy <17001873@gmail.com>
2019-10-04 15:41:06 +0200

    hello i added a image

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.helloandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +17,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-      
+        hello = findViewById(R.id.button);
+        hey = findViewById(R.id.textView);
+        String message = "lets win those stickers ";
+
+        hello.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                hey.setText("");
+                Toast.makeText(MainActivity.this,"Team Git hub",Toast.LENGTH_LONG).show();
+            }
+        });
 
 
     }
